5af448794c34d4894255217a97e93a8342f5d004
Hairo <hairo.f@yandex.ru>
2021-10-01 23:55:51 +0500

    Code Splitting

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,24 @@
 'use strict';
+import "./css/style.css";
+import Icon from './assets/img/logo512.png';
+import printMe from "./print";
 
 function component() {
     const element = document.createElement('div');
+    const button = document.createElement('button');
 
     // Lodash, currently included via a script, is required for this line to work
     element.textContent = 'Hello webpack';
+    button.textContent = 'Click me and check the console';
 
+    button.addEventListener('click', printMe);
+
+    const myIcon = new Image();
+    myIcon.src = Icon;
+
+    element.append(myIcon);
+    element.append(button);
+    
     return element;
 }
 
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,10 +1,61 @@
 const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = !isDev;
+const filename = ext => isDev ? `[name].${ext}` : `[name].[hash].${ext}`;
+
+const cssLoaders = extra => {
+    const loaders = [{
+            loader: MiniCssExtractPlugin.loader,
+        },
+        'css-loader'
+    ];
+    if (extra) {
+        loaders.push(extra);
+    }
+
+    return loaders;
+};
 
 module.exports = {
-    entry: './src/index.js',
     mode: 'development',
+    entry: {
+        index: './src/index.js',
+        print: './src/print.js'
+    },
+    devServer: {
+        static: './dist',
+    },
     output: {
-        filename: 'main.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            title: "HTML5 && CSS3"
+        }),
+        new MiniCssExtractPlugin({
+            filename: filename('css')
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: cssLoaders(),
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+    optimization: {
+        splitChunks: {
+          chunks: 'all',
+        },
     },
 }
