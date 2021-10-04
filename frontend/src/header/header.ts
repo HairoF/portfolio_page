dab22505be4a3ea894faae17ca4a061e82fb1661
Hairo <hairo.f@yandex.ru>
2021-10-04 20:47:56 +0500

    responsive header v2

--- frontend/src/header/header.ts
+++ frontend/src/header/header.ts
@@ -1,12 +1,18 @@
 import './header.scss';
-
+const burger = document.querySelector('.burger') as HTMLDivElement;
+const menuList = document.querySelector('.menu__list') as HTMLUListElement;
 export default function fromHeader():void {
-    const burger = document.querySelector('.burger') as HTMLDivElement;
-    burger.addEventListener('click', function(event:MouseEvent) {
+    burger.addEventListener('click', toggleBurger);
+    menuList.addEventListener('click', toggleBurger);
+}
+
+function toggleBurger(event:MouseEvent) {
         
-        burger.classList.toggle('burger_open');
+    burger.classList.toggle('burger_open');
+
+    const headerNav = document.querySelector('.header__nav') as HTMLElement;
+        headerNav.classList.toggle('header__nav_mobile');
         
-        const navbarMenu = document.querySelector('.navbar__menu');
+    const navbarMenu = document.querySelector('.navbar__menu');
         navbarMenu.classList.toggle('navbar__menu_mobile');
-    })
 }
